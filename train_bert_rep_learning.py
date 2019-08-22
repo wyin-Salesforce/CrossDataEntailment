3816826da0623e64b79066df8f5ffb7a2850e620
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-08-22 14:12:22 -0700

    first runable

--- train_bert_rep_learning.py
+++ train_bert_rep_learning.py
@@ -46,7 +46,7 @@ class Encoder(nn.Module):
             emb_batch.append(sent_pair_to_embedding(sent_pair[0], sent_pair[1], self.bert_tokenizer, self.bert_model, False).reshape(1,-1))
         bert_rep_batch = torch.cat(emb_batch, 0) #(batch, 768)
         batch_scores = (self.label_rep(bert_rep_batch)).tanh()#(batch, 2)
-        batch_probs = nn.Softmax(dim=1)((self.classifier(bert_rep_batch)).tanh())#(batch, 2)
+        batch_probs = nn.Softmax(dim=1)((self.classifier(bert_rep_batch)))#(batch, 2)
 
         return batch_scores, batch_probs
 

c92189aea44738449b2a9229507d3e3fdf48a55c
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-10 13:22:35 -0700

    coding

--- train_RTE_test_RTE.py
+++ train_RTE_test_RTE.py
@@ -38,15 +38,15 @@ from scipy.stats import pearsonr, spearmanr
 from sklearn.metrics import matthews_corrcoef, f1_score
 
 
-# from my_pytorch_transformers.modeling_bert import BertConfig#, BertForSequenceClassification
-# from my_pytorch_transformers.tokenization_bert import BertTokenizer
-# from my_pytorch_transformers.optimization import AdamW
-from my_pytorch_transformers.modeling_bert import  BertForSequenceClassification
+
+from my_pytorch_transformers.tokenization_bert import BertTokenizer
+from my_pytorch_transformers.optimization import AdamW
+# from my_pytorch_transformers.modeling_bert import  BertForSequenceClassification
 
 
-from pytorch_transformers.modeling_bert import BertConfig#, BertForSequenceClassification
-from pytorch_transformers.tokenization_bert import BertTokenizer
-from pytorch_transformers.optimization import AdamW
+from pytorch_transformers.modeling_bert import BertForSequenceClassification
+# from pytorch_transformers.tokenization_bert import BertTokenizer
+# from pytorch_transformers.optimization import AdamW
 
 
 # from bert_common_functions import BertForSequenceClassification

8b63d1870ee0ddc65759560e51c4e66d0cf46be4
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-10 15:35:13 -0700

    coding

--- train_RTE_test_RTE.py
+++ train_RTE_test_RTE.py
@@ -41,10 +41,10 @@ from sklearn.metrics import matthews_corrcoef, f1_score
 
 from pytorch_transformers.tokenization_bert import BertTokenizer
 from pytorch_transformers.optimization import AdamW
-from pytorch_transformers.modeling_bert import BertForSequenceClassification
+# from pytorch_transformers.modeling_bert import BertForSequenceClassification
 
 
-# from bert_common_functions import BertForSequenceClassification
+from bert_common_functions import BertForSequenceClassification
 
 
 

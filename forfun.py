2597127b1ad9b5ce64a4960aa8a82017706bd39a
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-11 11:35:56 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -29,7 +29,7 @@ from pytorch_transformers.modeling_roberta import RobertaForSequenceClassificati
 
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 model = RobertaModel.from_pretrained('roberta-base')
-input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
 print('input_ids:', input_ids)
 outputs = model(input_ids)
 last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple

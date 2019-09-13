f56f7231055bb4d38a4f1b9f51a5e6532af729f7
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-13 11:14:45 -0700

    first runable RoBERTa on FEVER test on FEVER

--- train_FEVER_test_FEVER_RoBERTa.py
+++ train_FEVER_test_FEVER_RoBERTa.py
@@ -113,32 +113,7 @@ class DataProcessor(object):
 
 class RteProcessor(DataProcessor):
     """Processor for the RTE data set (GLUE version)."""
-    def get_MNLI_as_train(self, filename):
-        '''
-        can read the training file, dev and test file
-        '''
-        examples=[]
-        readfile = codecs.open(filename, 'r', 'utf-8')
-        line_co=0
-        for row in readfile:
-            if line_co>0:
-                line=row.strip().split('\t')
-                guid = "train-"+str(line_co-1)
-                text_a = line[8].strip()
-                text_b = line[9].strip()
-                label = line[-1].strip() #["entailment", "neutral", "contradiction"]
-                examples.append(
-                    InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
-            line_co+=1
-            # if line_co > 20000:
-            #     break
-        readfile.close()
-        print('loaded  size:', line_co)
-        return examples
-
-
-
-    def get_FEVER_as_test(self, filename):
+    def get_FEVER_as_train_or_test(self, filename):
         readfile = codecs.open(filename, 'r', 'utf-8')
         line_co=0
         examples=[]
@@ -149,18 +124,18 @@ class RteProcessor(DataProcessor):
                 text_a = line[0].strip()
                 text_b = line[1].strip()
                 '''for RTE, we currently only choose randomly two labels in the set, in prediction we then decide the predicted labels'''
-                label = 'entailment'  if line[2] == 'entailment' else 'neutral'
+                label = 'entailment'  if line[2] == 'entailment' else 'not_entailment'
                 examples.append(
                     InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
                 line_co+=1
 
         readfile.close()
-        print('loaded test size:', line_co)
+        print('loaded  size:', line_co)
         return examples
 
     def get_labels(self):
         'here we keep the three-way in MNLI training '
-        return ["entailment", "neutral", "contradiction"]
+        return ["entailment", "not_entailment"]
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
@@ -471,7 +446,7 @@ def main():
     train_examples = None
     num_train_optimization_steps = None
     if args.do_train:
-        train_examples = processor.get_MNLI_as_train('/export/home/Dataset/glue_data/MNLI/train.tsv') #train_pu_half_v1.txt
+        train_examples = processor.get_FEVER_as_train_or_test('/export/home/Dataset/FEVER_2_Entailment/train.txt') #train_pu_half_v1.txt
         # seen_classes=[0,2,4,6,8]
 
         num_train_optimization_steps = int(
@@ -535,7 +510,7 @@ def main():
             pad_token_segment_id=0)#4 if args.model_type in ['xlnet'] else 0,)
 
         '''load dev set'''
-        eval_examples = processor.get_FEVER_as_test('/export/home/Dataset/FEVER_2_Entailment/test.txt')
+        eval_examples = processor.get_FEVER_as_train_or_test('/export/home/Dataset/FEVER_2_Entailment/test.txt')
         eval_features = convert_examples_to_features(
             eval_examples, label_list, args.max_seq_length, tokenizer, output_mode,
             cls_token_at_end=False,#bool(args.model_type in ['xlnet']),            # xlnet has a cls token at the end
@@ -641,10 +616,8 @@ def main():
                     wenpeng added a softxmax so that each row is a prob vec
                     '''
                     pred_probs = softmax(preds,axis=1)
-                    pred_indices = np.argmax(pred_probs, axis=1)
-                    pred_label_ids = []
-                    for p in pred_indices:
-                        pred_label_ids.append(0 if p == 0 else 1)
+                    pred_label_ids = list(np.argmax(pred_probs, axis=1))
+
                     gold_label_ids = gold_label_ids
                     assert len(pred_label_ids) == len(gold_label_ids)
                     hit_co = 0
@@ -652,7 +625,6 @@ def main():
                         if pred_label_ids[k] == gold_label_ids[k]:
                             hit_co +=1
                     test_acc = hit_co/len(gold_label_ids)
-
 
                     # test_acc = mean_f1#result.get("f1")
                     if test_acc > max_test_acc:

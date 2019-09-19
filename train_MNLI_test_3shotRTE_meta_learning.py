a9eb08e505eb62059920a5e8f81db4944840f195
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-19 10:31:46 -0700

    first runable meta learning

--- train_MNLI_test_3shotRTE_meta_learning.py
+++ train_MNLI_test_3shotRTE_meta_learning.py
@@ -712,7 +712,7 @@ def main():
                 assert input_ids.shape[0] == args.train_batch_size
 
                 mnli_entail_batch = get_a_random_batch_from_dataloader(MNLI_entail_dataloader)
-                print('random batch len:', len(mnli_entail_batch))
+                print('random batch len:', len(mnli_entail_batch[0]))
                 mnli_entail_batch_input_ids, mnli_entail_batch_input_mask, mnli_entail_batch_segment_ids, mnli_entail_batch_label_ids = tuple(t.to(device) for t in mnli_entail_batch) #mnli_entail_batch
                 print('sample entail:', mnli_entail_batch_input_ids.shape[0], mnli_entail_batch_label_ids.shape, mnli_entail_batch_label_ids)
 

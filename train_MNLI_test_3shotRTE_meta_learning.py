e86b2c988c894792d13f10eb230d1bc655acb495
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-19 13:02:27 -0700

    first runable meta learning

--- train_MNLI_test_3shotRTE_meta_learning.py
+++ train_MNLI_test_3shotRTE_meta_learning.py
@@ -27,7 +27,7 @@ from sklearn.metrics import matthews_corrcoef, f1_score
 
 from pytorch_transformers.tokenization_roberta import RobertaTokenizer
 from pytorch_transformers.optimization import AdamW
-from pytorch_transformers.modeling_roberta import RobertaModel, RobertaConfig, RobertaClassificationHead
+from pytorch_transformers.modeling_roberta import RobertaModel, RobertaConfig#, RobertaClassificationHead
 from pytorch_transformers.modeling_bert import BertPreTrainedModel
 
 from bert_common_functions import store_transformers_models, get_a_random_batch_from_dataloader
@@ -433,7 +433,23 @@ class Encoder(BertPreTrainedModel):
             return logits
 
 
+class RobertaClassificationHead(nn.Module):
+    """Head for sentence-level classification tasks."""
 
+    def __init__(self, config):
+        super(RobertaClassificationHead, self).__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, features, **kwargs):
+        x = features#[:, 0, :]  # take <s> token (equiv. to [CLS])
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.out_proj(x)
+        return x
 
 
 def main():

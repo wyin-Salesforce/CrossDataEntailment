d5940e0754a749eb0ced1786800f3f67d647ca24
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-19 04:19:19 -0700

    coding

--- train_MNLI_test_3shotRTE_meta_learning.py
+++ train_MNLI_test_3shotRTE_meta_learning.py
@@ -1,23 +1,4 @@
 
-from pytorch_transformers.tokenization_roberta import RobertaTokenizer
-from pytorch_transformers.optimization import AdamW
-from pytorch_transformers.modeling_roberta import RobertaModel
-
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """BERT finetuning runner."""
 
 from __future__ import absolute_import, division, print_function
@@ -46,7 +27,7 @@ from sklearn.metrics import matthews_corrcoef, f1_score
 
 from pytorch_transformers.tokenization_roberta import RobertaTokenizer
 from pytorch_transformers.optimization import AdamW
-from pytorch_transformers.modeling_roberta import RobertaForSequenceClassification
+from pytorch_transformers.modeling_roberta import RobertaModel
 
 from bert_common_functions import store_transformers_models, get_a_random_batch_from_dataloader
 

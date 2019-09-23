6d2df147cf3f12f8d46984e020bb0d9153d9df17
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-23 13:23:14 -0700

    coding

--- train_RTE_test_RTE_RoBERTa_fewshot.py
+++ train_RTE_test_RTE_RoBERTa_fewshot.py
@@ -151,7 +151,7 @@ class RteProcessor(DataProcessor):
                 text_a = line[1].strip()
                 text_b = line[2].strip()
                 label = line[3].strip() #["entailment", "not_entailment"]
-                if class2size.get(label, 0) < 3:
+                if class2size.get(label, 0) < 1:
                     examples.append(
                         InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
                     class2size[label]+=1

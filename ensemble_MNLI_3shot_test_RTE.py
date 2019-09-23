6df3e2aff5009db21e1241aa02e17c9e80702427
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-23 14:27:51 -0700

    coding

--- ensemble_MNLI_3shot_test_RTE.py
+++ ensemble_MNLI_3shot_test_RTE.py
@@ -490,12 +490,12 @@ def main():
     num_labels = len(label_list)
 
 
-    pretrain_model_dir = '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/trainMNLItestRTE/0.8409469823274425' #'roberta-large' , 'roberta-large-mnli'
+    pretrain_model_dir = '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/trainMNLItestRTE/0.8772563176895307' #'roberta-large' , 'roberta-large-mnli'
     model = RobertaForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=num_labels)
     tokenizer = RobertaTokenizer.from_pretrained(pretrain_model_dir, do_lower_case=args.do_lower_case)
     model.to(device)
 
-    pretrain_model_dir_2 = '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/3shotRTE/0.8049349783261087' #'roberta-large' , 'roberta-large-mnli'
+    pretrain_model_dir_2 = '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/3shotRTE/0.8339350180505415' #'roberta-large' , 'roberta-large-mnli'
     model_2 = RobertaForSequenceClassification.from_pretrained(pretrain_model_dir_2, num_labels=num_labels-1)
     tokenizer_2 = RobertaTokenizer.from_pretrained(pretrain_model_dir_2, do_lower_case=args.do_lower_case)
     model_2.to(device)

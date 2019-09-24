e10ebc3cca4b5d49f6813e8464e0d5d4f8ceccde
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-24 11:07:19 -0700

    coding

--- train_MNLI_test_3shotRTE_meta_learning.py
+++ train_MNLI_test_3shotRTE_meta_learning.py
@@ -929,7 +929,7 @@ def main():
                     prior_mnli_samples_logits = torch.mean(prior_mnli_samples_logits,dim=0)
 
                     '''second do few-shot training'''
-                    for ff in range(3*(check_freq//20)):
+                    for ff in range(3*(check_freq//10)):
                         model.train()
                         few_loss = model(eval_all_input_ids_shot.to(device), None, eval_all_input_mask_shot.to(device), sample_size=3, class_size =num_labels, labels=None, sample_labels = torch.cuda.LongTensor([0,0,0,1,1,1,2,2,2]), prior_samples_outputs = None, few_shot_training=True, is_train=True)
                         few_loss.backward()

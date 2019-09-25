8926bc33222b40a99e03ad96c6a3217e553bee77
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-25 00:41:42 -0700

    coding

--- train_MNLI_test_3shotRTE_meta_learning.py
+++ train_MNLI_test_3shotRTE_meta_learning.py
@@ -1019,7 +1019,7 @@ def main():
                         if idd == 0: # this is dev
                             if max(acc_list) > max_dev_acc:
                                 max_dev_acc = 0.5*(max(acc_list)+max_dev_acc)
-                                print('\ndev acc_list:', acc_list, ' max_dev_acc:', max_dev_acc, '\n')
+                                print('\ndev acc_list:', acc_list, ' max_mean_dev_acc:', max_dev_acc, '\n')
                                 '''store the model'''
                                 # store_transformers_models(model, tokenizer, '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/trainMNLItestRTE', str(max_dev_acc))
 
@@ -1027,8 +1027,8 @@ def main():
                                 print('\ndev acc_list:', acc_list, ' max_dev_acc:', max_dev_acc, '\n')
                                 break
                         else: # this is test
-                            if acc_list[-1] > max_test_acc:
-                                max_test_acc = acc_list[-1]
+                            if acc_list[-2] > max_test_acc:
+                                max_test_acc = acc_list[-2]
                             print('\ntest acc_list:', acc_list, ' max_test_acc:', max_test_acc, '\n')
 
 

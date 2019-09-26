3e6f796176d381bca3a76431644401035d3073df
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-26 02:32:40 -0700

    coding

--- train_MNLI_test_3shotRTE_meta_learning.py
+++ train_MNLI_test_3shotRTE_meta_learning.py
@@ -399,7 +399,7 @@ class Encoder(BertPreTrainedModel):
         '''
         # print('input_ids shape0 :', input_ids.shape[0])
         outputs = self.roberta(input_ids, token_type_ids, attention_mask) #(batch, max_len, hidden_size)
-        pooled_outputs = outputs[1]#torch.max(outputs[0],dim=1)[0]+ outputs[1]#outputs[1]#torch.mean(outputs[0],dim=1)#outputs[1] #(batch, hidden_size)
+        pooled_outputs = torch.max(outputs[0],dim=1)[0]#outputs[1]#torch.max(outputs[0],dim=1)[0]+ outputs[1]#outputs[1]#torch.mean(outputs[0],dim=1)#outputs[1] #(batch, hidden_size)
         LR_logits = self.classifier(pooled_outputs) #(9+batch, 3)
         if is_train:
 
@@ -596,7 +596,7 @@ def main():
                         action='store_true',
                         help="Set this flag if you are using an uncased model.")
     parser.add_argument("--train_batch_size",
-                        default=8,
+                        default=10,
                         type=int,
                         help="Total batch size for training.")
     parser.add_argument("--eval_batch_size",

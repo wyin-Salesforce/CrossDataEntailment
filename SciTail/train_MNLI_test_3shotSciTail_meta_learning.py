9bbd2ad3a0f025dfb6f9f065098be8e349a40c97
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-27 03:28:12 -0700

    coding

--- SciTail/train_MNLI_test_3shotSciTail_meta_learning.py
+++ SciTail/train_MNLI_test_3shotSciTail_meta_learning.py
@@ -211,8 +211,8 @@ class RteProcessor(DataProcessor):
                 examples.append(
                     InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
             line_co+=1
-            # if line_co > 20000:
-            #     break
+            if line_co > 20000:
+                break
         readfile.close()
         print('loaded  size:', line_co-1)
         return examples

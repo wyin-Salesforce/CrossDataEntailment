a9e828c2545b7334bdd49b60c50afdacce23a213
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-27 10:21:09 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -3,10 +3,10 @@
 # from transformers.optimization import AdamW
 from transformers.modeling_roberta import RobertaForSequenceClassification
 
-logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt = '%m/%d/%Y %H:%M:%S',
-                    level = logging.INFO)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+#                     datefmt = '%m/%d/%Y %H:%M:%S',
+#                     level = logging.INFO)
+# logger = logging.getLogger(__name__)
 
 pretrain_model_dir = 'roberta-large-mnli' #'roberta-large' , 'roberta-large-mnli'
 model = RobertaForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=2)

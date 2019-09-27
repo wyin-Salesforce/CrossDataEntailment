77885f18578d337010f07b5332afe74deaed9a73
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-09-27 10:26:31 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -1,12 +1,4 @@
-
-# from transformers.tokenization_roberta import RobertaTokenizer
-# from transformers.optimization import AdamW
-from transformers.modeling_roberta import RobertaForSequenceClassification
-
-# logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-#                     datefmt = '%m/%d/%Y %H:%M:%S',
-#                     level = logging.INFO)
-# logger = logging.getLogger(__name__)
+from pytorch_transformers.modeling_roberta import RobertaForSequenceClassification
 
 pretrain_model_dir = 'roberta-large-mnli' #'roberta-large' , 'roberta-large-mnli'
 model = RobertaForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=2)

6d7777103ec5bffe6a732cc132e2c59d0f7db566
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-10-03 16:03:40 -0700

    coding

--- SciTail/train_MNLI+3shotSciTail_test_SciTail_RoBERTa.py
+++ SciTail/train_MNLI+3shotSciTail_test_SciTail_RoBERTa.py
@@ -523,8 +523,8 @@ def main():
     # Prepare model
     # cache_dir = args.cache_dir if args.cache_dir else os.path.join(str(PYTORCH_TRANSFORMERS_CACHE), 'distributed_{}'.format(args.local_rank))
 
-    pretrain_model_dir = 'roberta-large-mnli' #'roberta-large' , 'roberta-large-mnli'
-    model = RobertaForSequenceClassification()#.from_pretrained(pretrain_model_dir, num_labels=num_labels)
+    pretrain_model_dir = 'roberta-large' #'roberta-large' , 'roberta-large-mnli'
+    model = RobertaForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=num_labels)
 
     # print(model.classifier.weight)
     # st_model = st_BertForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=num_labels)

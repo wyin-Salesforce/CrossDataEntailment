74413eb1121462ef81fcced24f05f9ed39c754b7
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-10-04 12:49:05 -0700

    coding

--- SciTail/train_SciTail_test_SciTail_RoBERTa.py
+++ SciTail/train_SciTail_test_SciTail_RoBERTa.py
@@ -151,7 +151,7 @@ class RteProcessor(DataProcessor):
                 text_a = line[0].strip()
                 text_b = line[1].strip()
                 random_value = random.uniform(0, 1)
-                if  random_value < 0.85:
+                if  random_value < 0.45:
                     continue
                 label = 'entailment'  if line[2] == 'entails' else 'neutral'
 
@@ -407,7 +407,7 @@ def main():
                         action='store_true',
                         help="Set this flag if you are using an uncased model.")
     parser.add_argument("--train_batch_size",
-                        default=20,
+                        default=2,
                         type=int,
                         help="Total batch size for training.")
     parser.add_argument("--eval_batch_size",
@@ -507,7 +507,7 @@ def main():
     train_examples = None
     num_train_optimization_steps = None
     if args.do_train:
-        train_examples = processor.get_SciTail_as_train('/export/home/Dataset/SciTailV1/tsv_format/scitail_1.0_train.tsv', sample_size=10) #train_pu_half_v1.txt
+        train_examples = processor.get_SciTail_as_train('/export/home/Dataset/SciTailV1/tsv_format/scitail_1.0_train.tsv', sample_size=1) #train_pu_half_v1.txt
         # seen_classes=[0,2,4,6,8]
 
         num_train_optimization_steps = int(

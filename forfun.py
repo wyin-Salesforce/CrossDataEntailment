fa097bafa5c46465d0e574ced6f6ef5a8c3aeb2c
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-10-11 09:22:33 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -1,4 +1,10 @@
-from pytorch_transformers.modeling_roberta import RobertaForSequenceClassification
+# from pytorch_transformers.modeling_roberta import RobertaForSequenceClassification
+#
+# pretrain_model_dir = 'roberta-large-mnli' #'roberta-large' , 'roberta-large-mnli'
+# model = RobertaForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=2)
 
-pretrain_model_dir = 'roberta-large-mnli' #'roberta-large' , 'roberta-large-mnli'
-model = RobertaForSequenceClassification.from_pretrained(pretrain_model_dir, num_labels=2)
+
+import random
+
+for x in range(10):
+    print(random.randint(0,10))

cd75a40bf8a135664ec79f24ffdd4ce2480b76cc
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-10-11 11:27:01 -0700

    coding

--- SciTail/train_MNLI_test_3shotSciTail_meta_learning_v2.py
+++ SciTail/train_MNLI_test_3shotSciTail_meta_learning_v2.py
@@ -705,7 +705,8 @@ def main():
     # Prepare model
     # cache_dir = args.cache_dir if args.cache_dir else os.path.join(str(PYTORCH_TRANSFORMERS_CACHE), 'distributed_{}'.format(args.local_rank))
 
-    pretrain_model_dir = 'roberta-large-mnli' #'roberta-large' , 'roberta-large-mnli'
+    # pretrain_model_dir = 'roberta-large-mnli' #'roberta-large' , 'roberta-large-mnli'
+    pretrain_model_dir = '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/trainMNLItestSciTail/0.8397239263803681'
     # model = Encoder.from_pretrained(pretrain_model_dir, num_labels=num_labels)
     model = Encoder.from_pretrained(pretrain_model_dir, num_labels=num_labels)
     # exit(0)

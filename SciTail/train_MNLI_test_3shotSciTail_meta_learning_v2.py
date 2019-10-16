72fa08a96436ecc58255a8dba1070517ac75cc5a
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2019-10-15 17:05:48 -0700

    coding

--- SciTail/train_MNLI_test_3shotSciTail_meta_learning_v2.py
+++ SciTail/train_MNLI_test_3shotSciTail_meta_learning_v2.py
@@ -384,7 +384,7 @@ class Encoder(BertPreTrainedModel):
         minibatch: input_ids, token_type_ids, attention_mask
         '''
         # print('input_ids shape0 :', input_ids.shape[0])
-        outputs = self.roberta(input_ids, token_type_ids, attention_mask) #(batch, max_len, hidden_size)
+        outputs = self.roberta(input_ids, attention_mask, token_type_ids) #(batch, max_len, hidden_size)
         pooled_outputs = outputs[1]#torch.max(outputs[0],dim=1)[0]+ outputs[1]#outputs[1]#torch.mean(outputs[0],dim=1)#outputs[1] #(batch, hidden_size)
         LR_logits = self.classifier(pooled_outputs) #(9+batch, 3)
         if is_train:

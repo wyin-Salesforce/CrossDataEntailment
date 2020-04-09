d7c9ea40b9d81d89b07743d474a3d44e4fd9c432
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-09 11:55:15 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -659,6 +659,8 @@ def main():
 
     # Prepare optimizer
     param_optimizer = list(model.named_parameters())
+    print('param_optimizer:', param_optimizer)
+    exit(0)
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},

f0ef46d401efba6e9be91a73b7241b2684c73241
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-11 17:21:35 -0700

    coding

--- 2020_train_MNLI_k_shot_RTE_myStilts.py
+++ 2020_train_MNLI_k_shot_RTE_myStilts.py
@@ -751,10 +751,10 @@ def main():
                 # with torch.no_grad():
                 logits_from_source_side, sequence_output_from_source_side = roberta_model(train_target_input_ids_batch, train_target_input_mask_batch, None, labels=None)
                 sequence_output_from_source_side = Variable(sequence_output_from_source_side.data, requires_grad=False)
-                logits_from_source_side = Variable(logits_from_source_side.data, requires_grad=False)
+                logits_from_source_side = Variable(logits_from_source_side[0].data, requires_grad=False)
 
                 model.train()
-                loss_cross_domain = model(sequence_output_from_source_side, target_labels_batch, logits_from_source_side[0], loss_fct=loss_fct)
+                loss_cross_domain = model(sequence_output_from_source_side, target_labels_batch, logits_from_source_side, loss_fct=loss_fct)
                 print('loss_cross_domain:', loss_cross_domain)
                 loss_cross_domain.backward()
                 optimizer.step()

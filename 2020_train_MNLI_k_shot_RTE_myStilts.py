ca76fd312d148942ab66cfd7c76ba96997bd5040
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-11 17:14:46 -0700

    coding

--- 2020_train_MNLI_k_shot_RTE_myStilts.py
+++ 2020_train_MNLI_k_shot_RTE_myStilts.py
@@ -748,8 +748,8 @@ def main():
                 # target_id_type_mask_batch = (train_target_input_ids_shot, None, train_target_input_mask_shot)
                 target_labels_batch = train_target_label_ids_batch
 
-                with torch.no_grad():
-                    logits_from_source_side, sequence_output_from_source_side = roberta_model(train_target_input_ids_batch, train_target_input_mask_batch, None, labels=None)
+                # with torch.no_grad():
+                logits_from_source_side, sequence_output_from_source_side = roberta_model(train_target_input_ids_batch, train_target_input_mask_batch, None, labels=None)
                 # sequence_output_from_source_side = roberta_model.sequence_output[:,0,:]
                 # sequence_output_from_source_side = Variable(sequence_output_from_source_side.data, requires_grad=True)
 

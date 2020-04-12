2f830ea71b0b8d3f59e291ecbc6386e4bdee2061
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-12 13:56:03 -0700

    coding

--- 2020_train_MNLI_k_shot_RTE.py
+++ 2020_train_MNLI_k_shot_RTE.py
@@ -831,7 +831,7 @@ def main():
                             test_acc/=len(test_dataloader)
                             print('\t\t\t >>>>test acc:', test_acc)
                             '''store the model, because we can test after a max_dev acc reached'''
-                            store_transformers_models(model, tokenizer, '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/trainMNLI'+str(args.k_shot)+'shotRTE', str(max_dev_acc)+'-'+str(test_acc))
+                            # store_transformers_models(model, tokenizer, '/export/home/Dataset/BERT_pretrained_mine/crossdataentail/trainMNLI'+str(args.k_shot)+'shotRTE', str(max_dev_acc)+'-'+str(test_acc))
 
 if __name__ == "__main__":
     main()

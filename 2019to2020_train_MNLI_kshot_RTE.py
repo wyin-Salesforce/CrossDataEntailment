115d759f0656ee0d0507dca0a1b60db223b5563e
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-15 06:53:05 -0700

    last 4 layers for classification

--- 2019to2020_train_MNLI_kshot_RTE.py
+++ 2019to2020_train_MNLI_kshot_RTE.py
@@ -553,15 +553,27 @@ class RobertaClassificationHead_3_layers(nn.Module):
     def __init__(self, config):
         super(RobertaClassificationHead_3_layers, self).__init__()
         self.dense = nn.Linear(config.hidden_size*4, config.hidden_size)
+        self.dense2 = nn.Linear(config.hidden_size, config.hidden_size)
+        self.dense3 = nn.Linear(config.hidden_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
     def forward(self, features, **kwargs):
         x = features#[:, 0, :]  # take <s> token (equiv. to [CLS])
         x = self.dropout(x)
+        
         x = self.dense(x)
         x = torch.tanh(x)
         x = self.dropout(x)
+
+        x = self.dense2(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+
+        x = self.dense3(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+
         x = self.out_proj(x)
         return x
 

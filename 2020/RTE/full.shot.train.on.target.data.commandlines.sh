de1d3c39ecd48bc63ffb027a2164ff9f5f92432a
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-24 12:36:55 -0700

    coding

--- 2020/RTE/full.shot.train.on.target.data.commandlines.sh
+++ 2020/RTE/full.shot.train.on.target.data.commandlines.sh
@@ -1,11 +1,11 @@
-export SHOT=1 #1, 3, 5, 10, 100000
-export BATCHSIZE=2 #2, 3, 5, 5, 5
+export SHOT=10 #1, 3, 5, 10, 100000
+export BATCHSIZE=5 #2, 3, 5, 5, 5
 export EPOCHSIZE=20
 export LEARNINGRATE=1e-6
 
 
 
-CUDA_VISIBLE_DEVICES=5 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=0 python -u full.shot.train.on.target.data.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=5 python -u full.shot.train.on.target.data.py \
     --seed 42 \
     --kshot $SHOT > log.RTE.$SHOT.shot.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=6 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=1 python -u full.shot.train.on.target.data.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -29,7 +29,7 @@ CUDA_VISIBLE_DEVICES=6 python -u full.shot.train.on.target.data.py \
     --seed 16 \
     --kshot $SHOT > log.RTE.$SHOT.shot.seed.16.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=7 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=2 python -u full.shot.train.on.target.data.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -41,7 +41,7 @@ CUDA_VISIBLE_DEVICES=7 python -u full.shot.train.on.target.data.py \
     --seed 32 \
     --kshot $SHOT > log.RTE.$SHOT.shot.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=5 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=3 python -u full.shot.train.on.target.data.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -54,7 +54,7 @@ CUDA_VISIBLE_DEVICES=5 python -u full.shot.train.on.target.data.py \
     --kshot $SHOT > log.RTE.$SHOT.shot.seed.64.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=6 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=4 python -u full.shot.train.on.target.data.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
--- 2020/compute_mean_std.py
+++ 2020/compute_mean_std.py
@@ -5,7 +5,7 @@ import statistics
 
 
 # initializing list
-test_list = [50.71, 50.28, 50.31, 50.71, 49.71]
+test_list = [80.92, 80.72, 79.75, 79.35, 79.15]
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)

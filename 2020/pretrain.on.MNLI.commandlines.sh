24e23afd73b2823cf36a9f9a1b70f7c66587d91a
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-24 13:28:54 -0700

    coding

--- 2020/pretrain.on.MNLI.commandlines.sh
+++ 2020/pretrain.on.MNLI.commandlines.sh
@@ -1,67 +1,61 @@
-export SHOT=10 #1, 3, 5, 10, 100000
-export BATCHSIZE=2 #2, 3, 5, 2, 5
+export BATCHSIZE=16 #2, 3, 5, 2, 5
 export EPOCHSIZE=20
 export LEARNINGRATE=1e-6
 
 
 
-CUDA_VISIBLE_DEVICES=0 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=0 python -u pretrain.on.MNLI.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 42 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.42.txt 2>&1 &
+    --seed 42 > log.mnli.pretrain.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=1 python -u pretrain.on.MNLI.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 16 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.16.txt 2>&1 &
+    --seed 16 > log.mnli.pretrain.seed.16.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=2 python -u pretrain.on.MNLI.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 32 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.32.txt 2>&1 &
+    --seed 32 > log.mnli.pretrain.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=3 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=3 python -u pretrain.on.MNLI.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 64 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.64.txt 2>&1 &
+    --seed 64 > log.mnli.pretrain.seed.64.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=4 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=4 python -u pretrain.on.MNLI.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 128 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.128.txt 2>&1 &
+    --seed 128 > log.mnli.pretrain.seed.128.txt 2>&1 &

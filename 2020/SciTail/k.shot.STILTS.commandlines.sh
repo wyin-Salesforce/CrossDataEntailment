41fbf3a73c430bee65646e3440af10bd95a45431
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-25 12:43:59 -0700

    coding

--- 2020/SciTail/k.shot.STILTS.commandlines.sh
+++ 2020/SciTail/k.shot.STILTS.commandlines.sh
@@ -1,7 +1,7 @@
 export SHOT=10 #1, 3, 5, 10, 100000
 export BATCHSIZE=2 #2, 3, 5, 2, 5
 export EPOCHSIZE=10 #only need max 5 epochs
-export LEARNINGRATE=1e-6
+export LEARNINGRATE=1e-7
 
 
 
--- 2020/compute_mean_std.py
+++ 2020/compute_mean_std.py
@@ -5,7 +5,7 @@ import statistics
 
 
 # initializing list
-test_list = [81.98, 81.65, 83.20, 82.59, 81.89]
+test_list = [82.03, 82.64, 83.25, 81.56, 81.84]
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)

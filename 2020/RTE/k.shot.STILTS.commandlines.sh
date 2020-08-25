fa7baf5f1dc93581036616819abcf6bf48b138cc
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-25 12:54:29 -0700

    coding

--- 2020/RTE/k.shot.STILTS.commandlines.sh
+++ 2020/RTE/k.shot.STILTS.commandlines.sh
@@ -1,11 +1,11 @@
-export SHOT=1 #1, 3, 5, 10, 100000
-export BATCHSIZE=2 #2, 3, 5, 2, 5
+export SHOT=100000 #1, 3, 5, 10, 100000
+export BATCHSIZE=5 #2, 3, 5, 2, 5
 export EPOCHSIZE=10 #only need max 5 epochs
 export LEARNINGRATE=1e-6
 
 
 
-CUDA_VISIBLE_DEVICES=5 python -u k.shot.STILTS.py \
+CUDA_VISIBLE_DEVICES=0 python -u k.shot.STILTS.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=5 python -u k.shot.STILTS.py \
     --seed 42 \
     --kshot $SHOT > log.RTE.STILTS.$SHOT.shot.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=6 python -u k.shot.STILTS.py \
+CUDA_VISIBLE_DEVICES=1 python -u k.shot.STILTS.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -29,7 +29,7 @@ CUDA_VISIBLE_DEVICES=6 python -u k.shot.STILTS.py \
     --seed 16 \
     --kshot $SHOT > log.RTE.STILTS.$SHOT.shot.seed.16.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=7 python -u k.shot.STILTS.py \
+CUDA_VISIBLE_DEVICES=2 python -u k.shot.STILTS.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -41,7 +41,7 @@ CUDA_VISIBLE_DEVICES=7 python -u k.shot.STILTS.py \
     --seed 32 \
     --kshot $SHOT > log.RTE.STILTS.$SHOT.shot.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=5 python -u k.shot.STILTS.py \
+CUDA_VISIBLE_DEVICES=3 python -u k.shot.STILTS.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -54,7 +54,7 @@ CUDA_VISIBLE_DEVICES=5 python -u k.shot.STILTS.py \
     --kshot $SHOT > log.RTE.STILTS.$SHOT.shot.seed.64.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=6 python -u k.shot.STILTS.py \
+CUDA_VISIBLE_DEVICES=4 python -u k.shot.STILTS.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
--- 2020/compute_mean_std.py
+++ 2020/compute_mean_std.py
@@ -5,7 +5,7 @@ import statistics
 
 
 # initializing list
-test_list = [82.03, 82.64, 83.25, 81.56, 81.84]
+test_list = [81.84, 81.60, 81.98, 81.56, 81.84]
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)

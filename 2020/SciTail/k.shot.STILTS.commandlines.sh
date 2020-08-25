7bfedc068e860fdbaefa3ffcb176fd12746cb54e
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-25 13:59:26 -0700

    coding

--- 2020/SciTail/k.shot.STILTS.commandlines.sh
+++ 2020/SciTail/k.shot.STILTS.commandlines.sh
@@ -5,43 +5,7 @@ export LEARNINGRATE=1e-6
 
 
 
-CUDA_VISIBLE_DEVICES=5 python -u k.shot.STILTS.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 128 \
-    --seed 42 \
-    --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.42.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=6 python -u k.shot.STILTS.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 128 \
-    --seed 16 \
-    --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.16.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=7 python -u k.shot.STILTS.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 128 \
-    --seed 32 \
-    --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.32.txt 2>&1 &
-
-# CUDA_VISIBLE_DEVICES=3 python -u k.shot.STILTS.py \
+# CUDA_VISIBLE_DEVICES=5 python -u k.shot.STILTS.py \
 #     --task_name rte \
 #     --do_train \
 #     --do_lower_case \
@@ -50,11 +14,10 @@ CUDA_VISIBLE_DEVICES=7 python -u k.shot.STILTS.py \
 #     --eval_batch_size 64 \
 #     --learning_rate $LEARNINGRATE \
 #     --max_seq_length 128 \
-#     --seed 64 \
-#     --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.64.txt 2>&1 &
+#     --seed 42 \
+#     --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.42.txt 2>&1 &
 #
-#
-# CUDA_VISIBLE_DEVICES=4 python -u k.shot.STILTS.py \
+# CUDA_VISIBLE_DEVICES=6 python -u k.shot.STILTS.py \
 #     --task_name rte \
 #     --do_train \
 #     --do_lower_case \
@@ -63,5 +26,42 @@ CUDA_VISIBLE_DEVICES=7 python -u k.shot.STILTS.py \
 #     --eval_batch_size 64 \
 #     --learning_rate $LEARNINGRATE \
 #     --max_seq_length 128 \
-#     --seed 128 \
-#     --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.128.txt 2>&1 &
+#     --seed 16 \
+#     --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.16.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=7 python -u k.shot.STILTS.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length 128 \
+#     --seed 32 \
+#     --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.32.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=3 python -u k.shot.STILTS.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 64 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length 128 \
+    --seed 64 \
+    --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.64.txt 2>&1 &
+
+
+CUDA_VISIBLE_DEVICES=4 python -u k.shot.STILTS.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 64 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length 128 \
+    --seed 128 \
+    --kshot $SHOT > log.SciTail.STILTS.$SHOT.shot.seed.128.txt 2>&1 &
--- 2020/compute_mean_std.py
+++ 2020/compute_mean_std.py
@@ -5,7 +5,7 @@ import statistics
 
 
 # initializing list
-test_list = [81.84, 81.60, 81.98, 81.56, 81.84]
+test_list = [86.26, 86.19, 86.69, 86.02, 86.16]
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)

b47ffd09b5c12cfd3e7c071660861c5132be38b1
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-26 21:48:58 -0700

    coding

--- 2020/RTE/k.shot.prototype.net.commandlines.sh
+++ 2020/RTE/k.shot.prototype.net.commandlines.sh
@@ -1,11 +1,11 @@
-export SHOT=3 #1, 3, 5, 10, 100000
+export SHOT=1 #1, 3, 5, 10, 100000
 export BATCHSIZE=32 #2, 3, 5, 2, 5
 export EPOCHSIZE=5 #only need max 5 epochs
 export LEARNINGRATE=1e-6
 
 
 
-CUDA_VISIBLE_DEVICES=0 python -u k.shot.prototype.net.py \
+CUDA_VISIBLE_DEVICES=5 python -u k.shot.prototype.net.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -15,7 +15,7 @@ CUDA_VISIBLE_DEVICES=0 python -u k.shot.prototype.net.py \
     --seed 42 \
     --kshot $SHOT > log.RTE.PrototypeNet.$SHOT.shot.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u k.shot.prototype.net.py \
+CUDA_VISIBLE_DEVICES=6 python -u k.shot.prototype.net.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -25,7 +25,7 @@ CUDA_VISIBLE_DEVICES=1 python -u k.shot.prototype.net.py \
     --seed 16 \
     --kshot $SHOT > log.RTE.PrototypeNet.$SHOT.shot.seed.16.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python -u k.shot.prototype.net.py \
+CUDA_VISIBLE_DEVICES=7 python -u k.shot.prototype.net.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -35,7 +35,7 @@ CUDA_VISIBLE_DEVICES=2 python -u k.shot.prototype.net.py \
     --seed 32 \
     --kshot $SHOT > log.RTE.PrototypeNet.$SHOT.shot.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=3 python -u k.shot.prototype.net.py \
+CUDA_VISIBLE_DEVICES=5 python -u k.shot.prototype.net.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -46,7 +46,7 @@ CUDA_VISIBLE_DEVICES=3 python -u k.shot.prototype.net.py \
     --kshot $SHOT > log.RTE.PrototypeNet.$SHOT.shot.seed.64.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=4 python -u k.shot.prototype.net.py \
+CUDA_VISIBLE_DEVICES=6 python -u k.shot.prototype.net.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \

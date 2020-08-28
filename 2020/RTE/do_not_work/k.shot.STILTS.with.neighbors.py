11fd66cabc6e580a19edfef34542948c3c3718f0
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-28 15:02:15 -0700

    coding

--- 2020/RTE/do_not_work/k.shot.STILTS.with.neighbors.py
+++ 2020/RTE/do_not_work/k.shot.STILTS.with.neighbors.py
@@ -59,6 +59,18 @@ logger = logging.getLogger(__name__)
 bert_hidden_dim = 1024
 pretrain_model_dir = 'roberta-large' #'roberta-large' , 'roberta-large-mnli', 'bert-large-uncased'
 
+def store_transformers_models(model, tokenizer, output_dir, flag_str):
+    '''
+    store the model
+    '''
+    output_dir+='/'+flag_str
+    # if not os.path.exists(output_dir):
+    #     os.makedirs(output_dir)
+    print('starting model storing....')
+    # model.save_pretrained(output_dir)
+    torch.save(model.state_dict(), output_dir)
+    # tokenizer.save_pretrained(output_dir)
+    print('store succeed')
 
 class RobertaForSequenceClassification(nn.Module):
     def __init__(self, tagset_size):

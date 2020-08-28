a4dca5fa78fb6d6074cf3df3d45d74447bcf1a7b
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-28 14:57:39 -0700

    coding

--- 2020/RTE/do_not_work/k.shot.STILTS.with.neighbors.py
+++ 2020/RTE/do_not_work/k.shot.STILTS.with.neighbors.py
@@ -685,11 +685,11 @@ def main():
     param_optimizer_finetune = list(model.named_parameters())
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        {'params': [p for n, p in param_optimizer_finetune if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer_finetune if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
         ]
 
-    optimizer_finetune = AdamW(param_optimizer_finetune,
+    optimizer_finetune = AdamW(optimizer_grouped_parameters,
                              lr=args.learning_rate)
 
     global_step = 0

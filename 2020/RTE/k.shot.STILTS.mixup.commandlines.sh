1596b63a75015e690467442d3fb40801a8b87884
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-27 23:45:51 -0700

    coding

--- 2020/RTE/k.shot.STILTS.mixup.commandlines.sh
+++ 2020/RTE/k.shot.STILTS.mixup.commandlines.sh
@@ -2,7 +2,7 @@ export SHOT=10 #1, 3, 5, 10, 100000
 export BATCHSIZE=5 #2, 3, 5, 2, 5
 export EPOCHSIZE=10 #only need max 10 epochs
 export LEARNINGRATE=1e-6
-export LAMBDATIMES=15
+export LAMBDATIMES=30
 
 
 
--- 2020/compute_mean_std.py
+++ 2020/compute_mean_std.py
@@ -5,7 +5,7 @@ import statistics
 
 
 # initializing list
-test_list = [84.66, 83.89, 82.12, 84.19, 83.92]
+test_list = [84.62, 84.49, 82.72, 83.86, 83.99]
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)

e3ff2199c4dc90dc5f285a5378909207717936e9
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-31 13:05:56 -0700

    change

--- 2020/RTE/do_not_work/k.shot.prototype.advanced.py
+++ 2020/RTE/do_not_work/k.shot.prototype.advanced.py
@@ -23,7 +23,7 @@ from torch.nn import CrossEntropyLoss, MSELoss
 from scipy.special import softmax
 # from scipy.stats import pearsonr, spearmanr
 # from sklearn.metrics import matthews_corrcoef, f1_score
-
+from torch.nn import functional as F
 from transformers.tokenization_roberta import RobertaTokenizer
 from transformers.optimization import AdamW
 from transformers.modeling_roberta import RobertaModel#RobertaForSequenceClassification

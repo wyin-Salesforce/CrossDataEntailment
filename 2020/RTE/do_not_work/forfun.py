2e68f6acc4547a989680d9f6d850c37b796244b2
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-01 17:30:40 -0700

    coding

--- 2020/RTE/do_not_work/forfun.py
+++ 2020/RTE/do_not_work/forfun.py
@@ -675,15 +675,15 @@ def main():
     optimizer_pretrain = AdamW(optimizer_grouped_parameters_pretrain,
                              lr=5e-7)
 
-    param_optimizer = list(protonet.named_parameters())
-    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-    optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-        ]
-
-    optimizer = AdamW(optimizer_grouped_parameters,
-                             lr=args.learning_rate)
+    # param_optimizer = list(protonet.named_parameters())
+    # no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    # optimizer_grouped_parameters = [
+    #     {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    #     {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    #     ]
+    #
+    # optimizer = AdamW(optimizer_grouped_parameters,
+    #                          lr=args.learning_rate)
 
     global_step = 0
     nb_tr_steps = 0

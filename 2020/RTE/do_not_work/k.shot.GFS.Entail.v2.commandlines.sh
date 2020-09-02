a8d54924ef228c17fb3151cbd8f2099224e1597b
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-02 10:27:25 -0700

    coding

--- 2020/RTE/do_not_work/k.shot.GFS.Entail.v2.commandlines.sh
+++ 2020/RTE/do_not_work/k.shot.GFS.Entail.v2.commandlines.sh
@@ -1,12 +1,12 @@
 export SHOT=10 #1, 3, 5, 10, 100000
 export BATCHSIZE=32 #2, 3, 5, 2, 5
-export TARGETBATCHSIZE=4
+export TARGETBATCHSIZE=5
 export EPOCHSIZE=3 #only need max 5 epochs
 export LEARNINGRATE=1e-6
 
 
 
-CUDA_VISIBLE_DEVICES=5 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=0 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=5 python -u k.shot.GFS.Entail.v2.py \
     --seed 42 \
     --kshot $SHOT > log.RTE.GFS.Entail.v2.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=6 python -u k.shot.GFS.Entail.py \
+CUDA_VISIBLE_DEVICES=1 python -u k.shot.GFS.Entail.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -28,7 +28,7 @@ CUDA_VISIBLE_DEVICES=6 python -u k.shot.GFS.Entail.py \
     --seed 16 \
     --kshot $SHOT > log.RTE.GFS.Entail.v2.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.16.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=7 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=2 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -39,7 +39,7 @@ CUDA_VISIBLE_DEVICES=7 python -u k.shot.GFS.Entail.v2.py \
     --seed 32 \
     --kshot $SHOT > log.RTE.GFS.Entail.v2.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=5 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=3 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -51,7 +51,7 @@ CUDA_VISIBLE_DEVICES=5 python -u k.shot.GFS.Entail.v2.py \
     --kshot $SHOT > log.RTE.GFS.Entail.v2.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.64.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=6 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=4 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \

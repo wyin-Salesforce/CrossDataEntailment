e4bc9ad3744cdf44c9fbfae1ada3ba89ef863d8c
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-02 09:23:00 -0700

    coding

--- 2020/RTE/do_not_work/k.shot.GFS.Entail.v2.py
+++ 2020/RTE/do_not_work/k.shot.GFS.Entail.v2.py
@@ -803,7 +803,7 @@ def main():
                             logits = protonet(class_prototype_reps, last_hidden_target_batch)
 
                         '''combine with logits from source domain'''
-                        weight = 0.8
+                        weight = 0.7
                         logits = weight*logits+(1.0-weight)*logits_from_source
 
                         if len(preds) == 0:
@@ -853,7 +853,7 @@ if __name__ == "__main__":
     main()
 
 '''
-CUDA_VISIBLE_DEVICES=5 python -u k.shot.GFS.Entail.v2.py --do_lower_case --num_train_epochs 3 --train_batch_size 32 --eval_batch_size 64 --learning_rate 1e-6 --max_seq_length 128 --seed 42 --kshot 10
+CUDA_VISIBLE_DEVICES=6 python -u k.shot.GFS.Entail.v2.py --do_lower_case --num_train_epochs 3 --train_batch_size 32 --eval_batch_size 64 --learning_rate 1e-6 --max_seq_length 128 --seed 42 --kshot 10
 
 
 84.81/0.42

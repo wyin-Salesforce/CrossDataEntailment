c5df854f7ce1997ed0fdd43395abd320262bf7f4
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-02 22:14:41 -0700

    coding

--- 2020/RTE/do_not_work/k.shot.GFS.Entail.v2.commandlines.sh
+++ 2020/RTE/do_not_work/k.shot.GFS.Entail.v2.commandlines.sh
@@ -7,7 +7,7 @@ export DROPOUT=0.1
 
 
 
-CUDA_VISIBLE_DEVICES=0 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=3 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -16,9 +16,9 @@ CUDA_VISIBLE_DEVICES=0 python -u k.shot.GFS.Entail.v2.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
     --seed 42 \
-    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.42.txt 2>&1 &
+    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.bigmodel.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=4 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -27,9 +27,9 @@ CUDA_VISIBLE_DEVICES=1 python -u k.shot.GFS.Entail.v2.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
     --seed 16 \
-    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.16.txt 2>&1 &
+    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.bigmodel.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.16.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=5 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -38,9 +38,9 @@ CUDA_VISIBLE_DEVICES=2 python -u k.shot.GFS.Entail.v2.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
     --seed 32 \
-    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.32.txt 2>&1 &
+    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.bigmodel.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=7 python -u k.shot.GFS.Entail.v2.py \
+CUDA_VISIBLE_DEVICES=6 python -u k.shot.GFS.Entail.v2.py \
     --do_lower_case \
     --num_train_epochs $EPOCHSIZE \
     --train_batch_size $BATCHSIZE \
@@ -49,7 +49,7 @@ CUDA_VISIBLE_DEVICES=7 python -u k.shot.GFS.Entail.v2.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
     --seed 64 \
-    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.64.txt 2>&1 &
+    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.bigmodel.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.64.txt 2>&1 &
 
 
 CUDA_VISIBLE_DEVICES=7 python -u k.shot.GFS.Entail.v2.py \
@@ -61,4 +61,4 @@ CUDA_VISIBLE_DEVICES=7 python -u k.shot.GFS.Entail.v2.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
     --seed 128 \
-    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.128.txt 2>&1 &
+    --kshot $SHOT > log.RTE.GFS.Entail.v2.minus.bigmodel.drop.$DROPOUT.$TARGETBATCHSIZE.targetBatch.$SHOT.shot.seed.128.txt 2>&1 &

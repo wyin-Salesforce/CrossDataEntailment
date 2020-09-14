753873acb63ba339c5fea7196fee6a7a61805de4
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-14 15:28:27 -0700

    coding

--- 2020/SciTail/do_not_work/k.shot.GFS.Entail.py
+++ 2020/SciTail/do_not_work/k.shot.GFS.Entail.py
@@ -292,8 +292,8 @@ class PrototypeNet(nn.Module):
         score_matrix_to_fold = all_scores.view(-1, class_size) #(batch_size, class_size*2)
         # score_matrix = score_matrix_to_fold[:,:3]+score_matrix_to_fold[:, -3:]#(batch_size, class_size)
 
-        score_from_source = torch.sigmoid(self.score_proj(score_matrix_to_fold[:,:3]))
-        score_from_target = torch.sigmoid(self.score_proj(score_matrix_to_fold[:, -3:]))
+        score_from_source = score_matrix_to_fold[:,:3]#torch.sigmoid(self.score_proj(score_matrix_to_fold[:,:3]))
+        score_from_target = score_matrix_to_fold[:, -3:]#torch.sigmoid(self.score_proj(score_matrix_to_fold[:, -3:]))
         weight_4_highway = torch.sigmoid(self.score_proj_weight(score_matrix_to_fold))
         score_matrix = weight_4_highway*(score_from_source)+(1.0-weight_4_highway)*score_from_target
 

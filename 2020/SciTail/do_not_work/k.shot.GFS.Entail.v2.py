2741137dfa7efef7a4bc4137438d627838959190
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-16 23:43:36 -0700

    coding

--- 2020/SciTail/do_not_work/k.shot.GFS.Entail.v2.py
+++ 2020/SciTail/do_not_work/k.shot.GFS.Entail.v2.py
@@ -307,10 +307,10 @@ class PrototypeNet(nn.Module):
         '''integrate pretrain logits'''
 
         score_from_pretrain_sigmoid = torch.sigmoid(logits_from_pretrain)
-        score_from_pretrain = torch.sigmoid(self.score_proj_pretrain(score_from_pretrain_sigmoid))
+        score_from_pretrain = self.score_proj_pretrain(score_from_pretrain_sigmoid)
         # print('logits_from_pretrain:', logits_from_pretrain)
         # print('score_from_pretrain:', score_from_pretrain)
-        score_from_model = torch.sigmoid(self.score_proj_model(score_matrix))
+        score_from_model = self.score_proj_model(score_matrix)
         # print('score_matrix:', score_matrix)
         # print('score_from_model:', score_from_model)  #0-id最大
         weight_4_highway_pretrain = torch.sigmoid(self.score_proj_weight_pretrain(torch.cat([score_from_pretrain_sigmoid, score_from_model], dim=1)))

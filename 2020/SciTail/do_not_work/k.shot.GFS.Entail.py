f4205df533089822d3eeb670018f7aaee966e3ea
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-17 18:18:16 -0700

    coding

--- 2020/SciTail/do_not_work/k.shot.GFS.Entail.py
+++ 2020/SciTail/do_not_work/k.shot.GFS.Entail.py
@@ -709,6 +709,7 @@ def main():
     '''starting to train'''
     iter_co = 0
     final_test_performance = 0.0
+    loss_accu = 0.0
     for _ in trange(int(args.num_train_epochs), desc="Epoch"):
         tr_loss = 0
         nb_tr_examples, nb_tr_steps = 0, 0
@@ -743,8 +744,9 @@ def main():
             target_batch_logits = batch_logits[-target_last_hidden_batch.shape[0]:]
             target_loss_list = loss_by_logits_and_2way_labels(target_batch_logits, target_label_ids_batch.view(-1), device)
 
-            ratio=0.9
-            loss = source_loss_list+ratio*target_loss_list#torch.mean(torch.cat([source_loss_list, target_loss_list]))
+
+            loss = source_loss_list+target_loss_list#torch.mean(torch.cat([source_loss_list, target_loss_list]))
+            loss_accu+=loss
             if n_gpu > 1:
                 loss = loss.mean() # mean() to average on multi-gpu.
             if args.gradient_accumulation_steps > 1:
@@ -760,7 +762,8 @@ def main():
             optimizer.zero_grad()
             global_step += 1
             iter_co+=1
-            if iter_co %1==0:
+            print('iter_co:', iter_co, ' mean loss:', loss_accu/iter_co)
+            if iter_co > 100 and iter_co %1==0:
                 # if iter_co % len(source_remain_ex_dataloader)==0:
                 '''
                 start evaluate on dev set after this epoch

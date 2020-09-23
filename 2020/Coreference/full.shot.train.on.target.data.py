0928c632557d4e76c7303d17a6b5399c897a6745
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-23 11:14:19 -0700

    coding

--- 2020/Coreference/full.shot.train.on.target.data.py
+++ 2020/Coreference/full.shot.train.on.target.data.py
@@ -651,8 +651,7 @@ def main():
                             else:
                                 eval_output_list.append([example_prefix+str(example_id), False, True])
 
-                        print('eval_output_list:', eval_output_list)
-                        exit(0)
+
                         if idd == 0:
                             test_acc = run_scorer('/export/home/Dataset/gap_coreference/gap-validation.tsv', eval_output_list)
                         else:

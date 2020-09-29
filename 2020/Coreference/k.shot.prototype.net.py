7b678a69e83f74e92efd0a8bb0aa60f46fb5eaf0
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-28 22:46:44 -0700

    coding

--- 2020/Coreference/k.shot.prototype.net.py
+++ 2020/Coreference/k.shot.prototype.net.py
@@ -28,14 +28,16 @@ from transformers.tokenization_roberta import RobertaTokenizer
 from transformers.optimization import AdamW
 from transformers.modeling_roberta import RobertaModel#RobertaForSequenceClassification
 
+from preprocess import load_GAP_coreference_data
+from gap_scorer_modified import run_scorer
+
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt = '%m/%d/%Y %H:%M:%S',
                     level = logging.INFO)
 logger = logging.getLogger(__name__)
 
-# from pytorch_transformers.modeling_bert import BertPreTrainedModel, BertModel
-# import torch.nn as nn
+
 
 bert_hidden_dim = 1024
 pretrain_model_dir = 'roberta-large' #'roberta-large' , 'roberta-large-mnli', 'bert-large-uncased'

dc1c5e15a84c93cb3f978b586fb4d805853479f3
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-28 17:30:59 -0700

    coding

--- 2020/Coreference/k.shot.GFS.Entail.commandlines.sh
+++ 2020/Coreference/k.shot.GFS.Entail.commandlines.sh
@@ -7,53 +7,53 @@ export DROPOUT=0.1
 export UPDATEBERTLAYERS=5
 export MAXLEN=250
 
-CUDA_VISIBLE_DEVICES=0 python -u k.shot.GFS.Entail.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --target_train_batch_size $TARGETBATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 42 \
-    --update_BERT_top_layers $UPDATEBERTLAYERS \
-    --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.42.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=1 python -u k.shot.GFS.Entail.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --target_train_batch_size $TARGETBATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 16 \
-    --update_BERT_top_layers $UPDATEBERTLAYERS \
-    --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.16.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=2 python -u k.shot.GFS.Entail.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --target_train_batch_size $TARGETBATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 32 \
-    --update_BERT_top_layers $UPDATEBERTLAYERS \
-    --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.32.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=3 python -u k.shot.GFS.Entail.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --target_train_batch_size $TARGETBATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 64 \
-    --update_BERT_top_layers $UPDATEBERTLAYERS \
-    --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.64.txt 2>&1 &
+# CUDA_VISIBLE_DEVICES=0 python -u k.shot.GFS.Entail.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --target_train_batch_size $TARGETBATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 42 \
+#     --update_BERT_top_layers $UPDATEBERTLAYERS \
+#     --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.42.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=1 python -u k.shot.GFS.Entail.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --target_train_batch_size $TARGETBATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 16 \
+#     --update_BERT_top_layers $UPDATEBERTLAYERS \
+#     --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.16.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=2 python -u k.shot.GFS.Entail.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --target_train_batch_size $TARGETBATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 32 \
+#     --update_BERT_top_layers $UPDATEBERTLAYERS \
+#     --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.32.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=3 python -u k.shot.GFS.Entail.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --target_train_batch_size $TARGETBATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 64 \
+#     --update_BERT_top_layers $UPDATEBERTLAYERS \
+#     --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.64.txt 2>&1 &
 
 
 CUDA_VISIBLE_DEVICES=4 python -u k.shot.GFS.Entail.py \
@@ -64,6 +64,6 @@ CUDA_VISIBLE_DEVICES=4 python -u k.shot.GFS.Entail.py \
     --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length $MAXLEN \
-    --seed 128 \
+    --seed 256 \
     --update_BERT_top_layers $UPDATEBERTLAYERS \
     --kshot $SHOT > log.Coref.GFS.Entail.$SHOT.shot.seed.128.txt 2>&1 &

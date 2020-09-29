f167f5ea107cb0c05de5c2c6705476303f86f130
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-28 23:26:22 -0700

    coding

--- 2020/Coreference/k.shot.prototype.net.commandlines.sh
+++ 2020/Coreference/k.shot.prototype.net.commandlines.sh
@@ -5,15 +5,46 @@ export LEARNINGRATE=1e-6
 export MAXLEN=250
 
 
-CUDA_VISIBLE_DEVICES=7 python -u k.shot.prototype.net.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 42 \
-    --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.42.txt 2>&1 &
+# CUDA_VISIBLE_DEVICES=7 python -u k.shot.prototype.net.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 42 \
+#     --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.42.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=7 python -u k.shot.prototype.net.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 16 \
+#     --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.16.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=6 python -u k.shot.prototype.net.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 32 \
+#     --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.32.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=5 python -u k.shot.prototype.net.py \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 64 \
+#     --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.64.txt 2>&1 &
+
 
 CUDA_VISIBLE_DEVICES=7 python -u k.shot.prototype.net.py \
     --do_lower_case \
@@ -22,36 +53,5 @@ CUDA_VISIBLE_DEVICES=7 python -u k.shot.prototype.net.py \
     --eval_batch_size 64 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length $MAXLEN \
-    --seed 16 \
-    --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.16.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=6 python -u k.shot.prototype.net.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 32 \
-    --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.32.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=5 python -u k.shot.prototype.net.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 64 \
-    --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.64.txt 2>&1 &
-
-
-CUDA_VISIBLE_DEVICES=4 python -u k.shot.prototype.net.py \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 128 \
+    --seed 256 \
     --kshot $SHOT > log.Coref.PrototypeNet.$SHOT.shot.seed.128.txt 2>&1 &

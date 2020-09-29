7209f73e057a6cf57fc27bfa9426322ce57bd6c1
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-29 00:05:23 -0700

    start question answer

---
+++ 2020/QuestionAnswer/load_MCTest.py
@@ -0,0 +1,12 @@
+import xml.etree.ElementTree as ET
+
+def load_MCTest():
+    path  = '/export/home/Dataset/MCTest/Statements/'
+    tree = ET.parse(path+'mc500.train.statements.pairs')
+    root = tree.getroot()
+    for child in root:
+        print child
+        exit(0)
+
+if __name__ == "__main__":
+    load_MCTest()
--- 2020/compute_mean_std.py
+++ 2020/compute_mean_std.py
@@ -5,7 +5,8 @@ import statistics
 
 
 # initializing list
-test_list = [45.68, 57.13, 42.15]
+test_list = [74.95, 54.49, 30.58, 74.95, 74.58]
+
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)

9d9fa7c0c416b09574afe7de0f25b1f83b6bae72
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-29 00:11:22 -0700

    coding

--- 2020/QuestionAnswer/load_MCTest.py
+++ 2020/QuestionAnswer/load_MCTest.py
@@ -4,8 +4,13 @@ def load_MCTest():
     path  = '/export/home/Dataset/MCTest/Statements/'
     tree = ET.parse(path+'mc500.train.statements.pairs')
     root = tree.getroot()
-    for child in root:
-        print(child)
+    for pair in root.findall('pair'):
+        premise = pair.find('t').text
+        hypothesis = pair.find('h').text
+        label = pair.entailment
+        print(premise)
+        print(hypothesis)
+        print('label:', label)
         exit(0)
 
 if __name__ == "__main__":

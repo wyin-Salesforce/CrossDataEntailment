68e01270d5712b34a81798fe30fc225c2d56beba
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-09-29 23:36:36 -0700

    coding

--- 2020/QuestionAnswer/k.shot.GFS.Entail.py
+++ 2020/QuestionAnswer/k.shot.GFS.Entail.py
@@ -896,7 +896,7 @@ def main():
                         with torch.no_grad():
                             logits = protonet(class_prototype_reps, last_hidden_target_batch)
                         '''add source logits'''
-                        logits+=logits_from_source
+                        logits = F.softmax(logits, dim=1)+F.softmax(logits_from_source, dim=1)
                         if len(preds) == 0:
                             preds.append(logits.detach().cpu().numpy())
                         else:

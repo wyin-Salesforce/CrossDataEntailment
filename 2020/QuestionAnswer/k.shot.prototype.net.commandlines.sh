b8a5ec5f4b667f4bb75717807b1168054c22872d
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-10-01 12:38:35 -0700

    coding

--- 2020/QuestionAnswer/k.shot.prototype.net.commandlines.sh
+++ 2020/QuestionAnswer/k.shot.prototype.net.commandlines.sh
@@ -1,6 +1,6 @@
-export SHOT=1 #1, 3, 5, 10
+export SHOT=10 #1, 3, 5, 10
 export BATCHSIZE=32
-export EPOCHSIZE=5 #only need max 5 epochs
+export EPOCHSIZE=3 #only need max 5 epochs
 export LEARNINGRATE=1e-6
 export MAXLEN=250
 

cd323cec64244daf7e270bef448bf03a5659d974
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-10-01 02:21:04 -0700

    coding

--- 2020/QuestionAnswer/k.shot.prototype.net.py
+++ 2020/QuestionAnswer/k.shot.prototype.net.py
@@ -21,13 +21,11 @@ from tqdm import tqdm, trange
 from scipy.stats import beta
 from torch.nn import CrossEntropyLoss, MSELoss
 from scipy.special import softmax
-# from scipy.stats import pearsonr, spearmanr
-# from sklearn.metrics import matthews_corrcoef, f1_score
-
 from transformers.tokenization_roberta import RobertaTokenizer
 from transformers.optimization import AdamW
 from transformers.modeling_roberta import RobertaModel#RobertaForSequenceClassification
 
+from load_MCTest import load_MCTest
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt = '%m/%d/%Y %H:%M:%S',

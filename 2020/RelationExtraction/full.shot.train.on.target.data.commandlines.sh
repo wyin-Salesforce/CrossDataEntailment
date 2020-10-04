dadeb261ee2903e2e239805b7e39de018be68887
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-10-04 12:31:14 -0700

    coding

--- 2020/RelationExtraction/full.shot.train.on.target.data.commandlines.sh
+++ 2020/RelationExtraction/full.shot.train.on.target.data.commandlines.sh
@@ -1,47 +1,47 @@
-export SHOT=1 #1, 3, 5, 10, 0
+export SHOT=3 #1, 3, 5, 10, 0
 export BATCHSIZE=20 #2, 4, 4, 5, 16
 export EPOCHSIZE=3
 export LEARNINGRATE=1e-6
 export MAXLEN=128
 
 
-CUDA_VISIBLE_DEVICES=0 python -u full.shot.train.on.target.data.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 42 \
-    --kshot $SHOT > log.RE.$SHOT.shot.seed.42.txt 2>&1 &
+# CUDA_VISIBLE_DEVICES=0 python -u full.shot.train.on.target.data.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 42 \
+#     --kshot $SHOT > log.RE.$SHOT.shot.seed.42.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=1 python -u full.shot.train.on.target.data.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 16 \
+#     --kshot $SHOT > log.RE.$SHOT.shot.seed.16.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=2 python -u full.shot.train.on.target.data.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 64 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length $MAXLEN \
+#     --seed 32 \
+#     --kshot $SHOT > log.RE.$SHOT.shot.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u full.shot.train.on.target.data.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 16 \
-    --kshot $SHOT > log.RE.$SHOT.shot.seed.16.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=2 python -u full.shot.train.on.target.data.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length $MAXLEN \
-    --seed 32 \
-    --kshot $SHOT > log.RE.$SHOT.shot.seed.32.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=3 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=6 python -u full.shot.train.on.target.data.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -54,7 +54,7 @@ CUDA_VISIBLE_DEVICES=3 python -u full.shot.train.on.target.data.py \
     --kshot $SHOT > log.RE.$SHOT.shot.seed.64.txt 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=4 python -u full.shot.train.on.target.data.py \
+CUDA_VISIBLE_DEVICES=7 python -u full.shot.train.on.target.data.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
